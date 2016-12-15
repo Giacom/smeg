38cdc4bbb25376aac03b4fa301f437a14fc4934c
Giacom <giacomand@gmail.com>
2016-12-15 01:46:23 +0000

    Fixed the perspective flipping horizontally.

--- engine/src/service/viewport.cpp
+++ engine/src/service/viewport.cpp
@@ -6,9 +6,8 @@ namespace smeg {
 	}
 
 	void Viewport::UpdateViewport(int width, int height) {
-		//perspectiveMatrix = Matrix4::Ortho(0, width, height, 0, -1, 1);
-		perspectiveMatrix = Matrix4::Ortho(-(width / height), (width / height), 1.0f, -1.0f, -1.0f, 1.0f);
-		//perspectiveMatrix = Matrix4::Ortho(-4.0f/3.0f, 4.0f/3.0f, 1.0f, -1.0f, -1.0f, 1.0f);
+		perspectiveMatrix = Matrix4::Ortho((width / height), -(width / height), 1.0f, -1.0f, -1.0f, 1.0f);
+		//perspectiveMatrix = Matrix4::Identity();
 		size = Vector2(width, height);
 	}
 
--- sample/src/main.cpp
+++ sample/src/main.cpp
@@ -66,7 +66,7 @@ int main(int, char**) {
 
 	{
 		std::unique_ptr<Entity> entity = std::make_unique<Entity>();
-		std::unique_ptr<Component> transform = std::make_unique<Transform>(0, 0, 0, 0);
+		std::unique_ptr<Component> transform = std::make_unique<Transform>(0, 0, 0.1f, 0);
 		std::unique_ptr<Component> sprite = std::make_unique<Sprite>("res/duck.png", 128, 128, 100);
 
 		entity->AddComponent(transform);
